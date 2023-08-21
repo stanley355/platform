0a9d16f9ed78d2c24fa979bf57f6a645df4396a5
stan <winatastanley355@gmail.com>
2023-08-21 09:27:20 +0700

    Amazon best seller api

---
+++ src/pages/api/amazon/bestsellers.ts
@@ -0,0 +1,36 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import puppeteer from "puppeteer";
+
+const scraperAPI = async (req: NextApiRequest, res: NextApiResponse) => {
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+  const url = "https://www.amazon.com/gp/bestsellers/?ref_=navm_cs_bestsellers";
+  await page.goto(url, { waitUntil: "domcontentloaded" });
+
+  const data = await page.evaluate(() => {
+    const parents: any = document.querySelectorAll(
+      ".zg-carousel-general-faceout"
+    );
+
+    return [...parents].map((el: any) => {
+      const link: any = el.querySelector(".a-link-normal");
+      const img: any = el.querySelector(".a-dynamic-image");
+      const soldAmount: any = el.querySelector(".a-size-small");
+      const price: any = el.querySelector(".a-size-base > span");
+      return {
+        url: link.href,
+        imgSrc: img.src,
+        imgAlt: img.alt,
+        soldAmount: soldAmount.innerText,
+        price: Number(price.innerText.replace("$", "")),
+      };
+    });
+  });
+
+  await browser.close();
+  res.send(data);
+};
+
+export default scraperAPI;
